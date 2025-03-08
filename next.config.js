affd7783abe75e17e3f782898ed8f05a84aa906a
rockmonkey4004 <55054729+rockmonkey4004@users.noreply.github.com>
2025-03-08 13:07:39 -0600

    .

--- next.config.js
+++ next.config.js
@@ -13,9 +13,11 @@ const withMDX = require('@next/mdx')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   images: {
     domains: ['res.cloudinary.com'],
+    formats: ['image/avif', 'image/webp'],
   },
   // If you're using Netlify, this helps with redirects
   async redirects() {
@@ -28,6 +30,8 @@ const nextConfig = {
       },
     ];
   },
+  // To ensure better compatibility with Netlify
+  target: 'serverless',
 };
 
 module.exports = withMDX(nextConfig); 
